0be7564f21e592abc46accef8ecccaf92a2031f0
Vatsal Srivastava <srivastava.vatsal.2023@gmail.com>
2022-08-06 09:00:49 +0530

    copyright notices updated

--- src/links.py
+++ src/links.py
@@ -1,3 +1,23 @@
+"""
+This file contains the StickLink class which is used to construct links.
+Copyright (C) 2022-23 Vatsal Srivastava
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You can find a copy of the GNU General Public License in the root
+directory of this repository, named `LICENSE.md`.
+If you do not find it, see <https://www.gnu.org/licenses/>.
+"""
+
+
 from knots import *
 
 class StickLink:
